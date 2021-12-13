23da65e6a3843f342abee20ab25d3e59206d15e8
AminaLari <73361357+AminaLari@users.noreply.github.com>
2021-12-13 20:32:37 +0300

    Delete 6 лр/djangoProject5/artist directory

--- 6 лр/djangoProject5/artist/serializers.py
+++
@@ -1,7 +0,0 @@
-from artist.models import Artists
-from rest_framework import serializers
-
-class ArtistsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model= Artists
-        fields=['id','name','country','arts','is_impressionism']
--- 6 лр/djangoProject5/artist/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from artist.serializers import ArtistsSerializer
-from artist.models import Artists
-
-class ArtistsViewSet(viewsets.ModelViewSet):
-    queryset = Artists.objects.all()
-    serializer_class =ArtistsSerializer # Сериализатор для модели
-
