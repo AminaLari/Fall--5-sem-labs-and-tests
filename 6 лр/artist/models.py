5122dc5bc53dc67aaff62bea3fbb157226af7a7a
AminaLari <73361357+AminaLari@users.noreply.github.com>
2021-12-13 20:28:27 +0300

    Delete 6 лр/artist directory

--- 6 лр/artist/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Artists(models.Model):
-    name = models.CharField(max_length=50, verbose_name="Имя художника")
-    country=models.CharField(max_length=40, verbose_name="Страна")
-    arts=models.CharField(max_length=100, verbose_name="Известные работы")
-    is_impressionism = models.BooleanField(verbose_name="Художник работал в жанре импрессионизм?")
-
